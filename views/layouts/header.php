7173b0dff006f8bb0b62cc2fb8b22fe3d9bd83f9
Jonathan <rafaelyonathan1151@gmail.com>
2025-10-05 15:31:47 +0700

    Fix Header
    - Removing Search and some option

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -15,21 +15,7 @@ use yii\helpers\Html;
         </button>
 
         <div class="collapse navbar-collapse justify-content-end">
-            <form class="navbar-form">
-              <span class="bmd-form-group"><div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                  <i class="material-icons">search</i>
-                  <div class="ripple-container"></div>
-                </button>
-              </div></span>
-            </form>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#pablo">
-                        <i class="material-icons">dashboard</i>
-                    </a>
-                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="#pablo" id="navbarDropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="material-icons">person</i>
@@ -38,9 +24,6 @@ use yii\helpers\Html;
                         </p>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-                        <a class="dropdown-item" href="#">Profile</a>
-                        <a class="dropdown-item" href="#">Settings</a>
-                        <div class="dropdown-divider"></div>
                         <?=
                             Yii::$app->user->isGuest ? 
                                 Html::a(
